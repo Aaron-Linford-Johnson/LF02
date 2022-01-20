3ef28abd60a98cf1073102de09aca55fae3ab202
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2022-01-20 13:59:28 +0100

    Torwart

---
+++ Fußball/src/schritt1/Torwart.java
@@ -0,0 +1,22 @@
+package schritt1;
+
+public class Torwart extends Spieler{
+    private int reaktion;
+
+    public Torwart(String name, int alter, int staerke, int motivation, int reaktion){
+        super(name,alter,staerke,0,motivation,0);
+        this.reaktion = reaktion;
+    }
+
+    public String toString(){
+        return super.toString() + "\nReaktion: " + reaktion;
+    }
+
+    public int getReaktion() {
+        return reaktion;
+    }
+
+    public void setReaktion(int reaktion) {
+        this.reaktion = reaktion;
+    }
+}
