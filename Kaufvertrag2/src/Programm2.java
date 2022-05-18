7ae3626221474c448ba4990cecfa58c852bb02ee
Aaron-Linford-Johnson <Aaron.Linford.Johnson@gmail.com>
2022-05-18 11:32:17 +0200

    Kaufvertrag2

--- Kaufvertrag2/src/Programm2.java
+++ Kaufvertrag2/src/Programm2.java
@@ -1,9 +1,12 @@
+import buisnessObjects.Vertragspartner;
 import dao.VertragspartnerDAO;
 import dao.WareDAO;
 
 public class Programm2 {
     public static void main(String[] args) throws ClassNotFoundException {
         VertragspartnerDAO vertragspartnerDAO = new VertragspartnerDAO();
+
+        Vertragspartner vertragspartner = new VertragspartnerDAO().read("1234");
 
         System.out.println("super!!!!");
 
--- Kaufvertrag2/src/dao/VertragspartnerDAO.java
+++ Kaufvertrag2/src/dao/VertragspartnerDAO.java
@@ -1,5 +1,10 @@
 package dao;
 
+import buisnessObjects.Adresse;
+import buisnessObjects.Vertragspartner;
+
+import java.sql.*;
+
 public class VertragspartnerDAO {
    private final String CLASSNAME = "org.sqlite.JDBC";
    private final String CONNECTIONSTRING = "jdbc:sqlite:Kaufvertrag2/src/data/kaufvertrag.db";
@@ -7,4 +12,55 @@ public class VertragspartnerDAO {
    public VertragspartnerDAO() throws ClassNotFoundException {
        Class.forName(CLASSNAME);
    }
+
+    /**
+     * Liest einen Vertragspartner auf Basis seiner Ausweisnummer
+     * @param ausweisNr die Ausweisnummer
+     * @return Der gewünschte Vertragspartner
+     */
+
+    public Vertragspartner read(String ausweisNr) {
+        Vertragspartner vertragspartner = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        // Verbindung zu Datenbank herstellen
+        try {
+            connection = DriverManager.getConnection(CONNECTIONSTRING);
+
+            //SQL-Abfrage erstellen
+            String sql = "SELECT * FROM vertragspartner WHERE ausweisNr = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, ausweisNr);
+
+            //SQL-Abfrage ausführen
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //Zeiger auf den ersten Datensatz setzen
+            resultSet.next();
+
+            //ResultSet auswerten
+            String nr = resultSet.getString("ausweisNr");
+            String vorname = resultSet.getString("vorname");
+            String nachname = resultSet.getString("nachname");
+            String strasse = resultSet.getString("strasse");
+            String hausNr = resultSet.getString("hausNr");
+            String plz = resultSet.getString("plz");
+            String ort =  resultSet.getString("ort");
+
+            //Vertragspartner erstellen
+            vertragspartner = new Vertragspartner(vorname, nachname);
+            vertragspartner.setAusweisNr(nr);
+            vertragspartner.setAdresse(new Adresse(strasse,hausNr, plz,ort));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return vertragspartner;
+    }
 }
