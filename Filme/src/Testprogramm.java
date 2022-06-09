eaaa383df1830b041124b5431d0e1e35b4ca913f
aaron123 <Aaron.Linford.Johnson@gmail.com>
2022-06-09 14:58:34 +0200

    lesen und schreiben läuft

--- Filme/src/Testprogramm.java
+++ Filme/src/Testprogramm.java
@@ -3,12 +3,22 @@ import dao.Filmedao;
 
 import java.sql.SQLException;
 
+import buisnessObjects.FilmSerie;
+import dao.Filmedao;
+
+import java.sql.SQLException;
+
 public class Testprogramm {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         Filmedao filmedao = new Filmedao();
-         FilmSerie filmSerie = filmedao.read(1);
-        System.out.println(filmSerie.getGenre());
+        FilmSerie filmSerie = filmedao.read(13);
+        System.out.println(filmSerie);
+        //------------------------------------------------------------------------------
+        FilmSerie filmSerie1 = new FilmSerie("Tarzan","Tarzan","Kinderserie",2005,4.99,5,"720p",90);
+        filmedao.insertInto(filmSerie1);
+        //------------------------------------------------------------------------------
+
 
     }
 }
